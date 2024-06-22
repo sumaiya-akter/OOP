916c4f88205f038379c27cdf755cb3f71de4e199
sumaiya-akter <61377175+sumaiya-akter@users.noreply.github.com>
2024-06-22 21:04:00 +0500

    Add files via upload

---
+++ Lab-1 Test/code/B/T6ATester/src/main/java/com/mycompany/t6atester/FinalT6A.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.t6atester;
+
+/**
+ *
+ * @author ANIK
+ */
+public class FinalT6A{ 
+  public static int temp = 4;
+  private int sum;
+  private int y = 1;
+  public FinalT6A(int x, int p){
+    temp+=1;
+    y = temp - p;
+    sum = temp + x;
+    System.out.println(x + " " + y+ " " + sum);
+  }
+  public void methodA(){    
+    int x=0, y =0;
+    y = y + this.y; 
+  }
+}
---
+++ Lab-1 Test/code/B/T6ATester/src/main/java/com/mycompany/t6atester/T6ATester.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t6atester;
+
+/**
+ *
+ * @author ANIK
+ */
+public class T6ATester {
+
+    public static void main(String[] args) {
+    FinalT6A q1 = new FinalT6A(2,1);
+    q1.methodA();
+    FinalT6A q2 = new FinalT6A(3,5);
+    q2.methodA();  
+    q1 = new FinalT6A(5,7);
+    q1.methodA();
+    q2.methodA();
+  }
+
+}
+ 
