825ce8b4b12d70cfc9ed87d68491f0b32cb39048
sumaiya-akter <61377175+sumaiya-akter@users.noreply.github.com>
2024-06-30 12:56:12 +0600

    Add files via upload

---
+++ today/Arrayproblem/src/main/java/arrayproblem/Arrayproblem.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package arrayproblem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Arrayproblem {
+    
+
+    public static void main(String[] args) {
+        
+       
+       
+     Scanner sc = new Scanner (System.in);
+     int []a= new int[5];
+     for (int i=0; i<a.length;++i){
+     a[i]=sc.nextInt();
+     }
+       System.out.println(a);
+     
+    }
+}
